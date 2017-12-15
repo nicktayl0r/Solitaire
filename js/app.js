1f4256387653ffd50ddfdba1ec6b589e071244ee
Nick Taylor <nicholasjt22@gmail.com>
2017-12-14 18:08:35 -0800

    create tableau array

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
+/*----- constants -----*/
 var suit = ['spade', 'heart', 'club', 'diamond'];
 var rank = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 
+/*----- app's state (variables) -----*/
+var deck, stock, tableau, foundation, waste, activeCard;
+/*----- cached element references -----*/
 var tableauEl = document.getElementById('tableau');
 var foundationEl = document.getElementById("foundation");
 var wasteEl = document.getElementById("waste");
 var stockEl = document.getElementById("stock");
+/*----- event listeners -----*/
+//restart button just inits 
+/*----- functions -----*/
 
-var deck = [];
-var stock = [];
+function init() {
+    tableau = [[],[],[],[],[],[],[]];
+    foundation = [[],[],[],[]];
+    deck = [];
+    stock = [];
+    waste = [];
+    makeDeck();
+    shuffleDeck();
+    makeTableau();   
+}
 
+function render(){
+    //update arrays
+    // one div for each item in array
+    //check the win conditions
+}
 
 class Card {
     constructor(suit, rank) {
@@ -18,7 +38,6 @@ class Card {
         this.isVisible = false;
     }
 }
-
 
 function makeDeck(){
     for(s in suit) {	
@@ -34,8 +53,20 @@ function shuffleDeck() {
             var randI = Math.floor(Math.random()*(deck.length));
             stock.push(deck[randI]);
             deck.splice(randI, 1);
-            console.log(deck.length);
         }
     return stock;
 }
 
+function makeTableau() {
+    for(t in tableau) {
+        while(tableau[t].length <= t ){
+            tableau[t].push(stock.pop());
+        }
+        var lastCard = tableau[t][t];
+        lastCard.isActive = true;
+        lastCard.isVisible = true;
+    }
+}
+
+init();
+render();
