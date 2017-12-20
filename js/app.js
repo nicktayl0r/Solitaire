0fbebc42892e1926c45ee047f53169547f12bfe2
Nick Taylor <nicholasjt22@gmail.com>
2017-12-20 00:28:06 -0800

    update tableau logic

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 /*----- constants -----*/
 var suit = ['spades', 'hearts', 'clubs', 'diamonds'];
 var rank = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-
 /*----- app's state (variables) -----*/
-var deck, stock, tableau, foundation, waste, selectedCard;
+var deck, stock, tableau, foundation, waste;
 /*----- cached element references -----*/
 var tableauEl = document.getElementById('tableau');
 var foundationEl = document.getElementById("foundation");
@@ -15,11 +14,10 @@ stockEl.addEventListener('click', addWaste);
 document.getElementById('restart').addEventListener('click',init);
 tableauEl.addEventListener('click', addTableau);
 foundationEl.addEventListener('click', addFoundation);
-//restart button just inits
 /*----- functions -----*/
 function init() {
     deck = [];
-    activeCard =[];
+    activeCard = [];
     stock = [];
     waste = [];
     tableau = [[],[],[],[],[],[],[]];
@@ -32,17 +30,22 @@ function init() {
 function render(){
     var tChildren = tableauEl.children;
     console.log()
+    var h = 0;
     for(col in tableau){
         while(tChildren[col].firstChild) {
             tChildren[col].removeChild(tChildren[col].firstChild);
         }
+        var i=0;
         for(card in tableau[col]) {
+
            if(tableau[col][card].isActive){ 
-               tChildren[col].innerHTML = `${tChildren[col].innerHTML}<div class='cards'>${tableau[col][card].rank} of ${tableau[col][card].suit}</div>`;
+               tChildren[col].innerHTML = `${tChildren[col].innerHTML}<div id='${h}${i}'class='cards'>${tableau[col][card].rank} of ${tableau[col][card].suit}</div>`;
             } else { 
-                tChildren[col].innerHTML = `${tChildren[col].innerHTML}<div class='cards'>boi</div>`;
+                tChildren[col].innerHTML = `${tChildren[col].innerHTML}<div id='${h}${i}'class='cards'></div>`;
             }
+            i++;
         } 
+        h++;    
     }
     var count = 0;
     while(wasteEl.childElementCount>0){
@@ -65,16 +68,10 @@ class Card {
 	    this.isActive = false;
     }
 }
-
 function makeDeck(){
     for(s in suit) {	
 		for(r in rank) {
         deck.push(new Card(suit[s], rank[r]));
-        // deck.push({
-        //     suit: suit[s],
-        //     rank: rank[r],
-        //     isActive: false
-        // });
 		}
     }
     return deck;
@@ -97,27 +94,29 @@ function makeTableau() {
     }
 }
 function addFoundation(e) {
-    if(activeCard.length ===1){
-        var fTarget = foundation[e.target.id.charAt(1)];
-        console.log();
+    if(activeCard.length === 1){
+        var fTarget = foundation[e.target.id.charAt(0)];
         var isSuit = ((fTarget.length === 0) || (fTarget[fTarget.length-1].suit === activeCard[0].suit));
         var isRank = fTarget.length + 1 === activeCard[0].rank;
         if  (isRank && isSuit) {
-            fTarget.push(activeCard.pop());
+                fTarget.push(activeCard.shift());
         }
         console.log(foundation);
         render();    
     }
 }
 function addTableau(e) {
-    if(activeCard.length === 1) {
-        console.log(e.target.parentNode)
-        var tTarget = tableau[e.target.parentNode.id.charAt(1)];
+    if(activeCard.length >= 1) {
+        var tTarget = tableau[e.target.id.charAt(0)];
         var rankChk = activeCard[0].rank+1 === tTarget[tTarget.length-1].rank;
         var suitChk = suit.indexOf(activeCard[0].suit)%2 !== suit.indexOf(tTarget[tTarget.length-1].suit)%2;
-        
+        console.log(`activeCard rank is ${activeCard[0].rank}`);
+        console.log(`rank check returns ${rankChk}`);
+        console.log(`suitChk returns ${suitChk}`);
         if((activeCard[0].rank === 13 && tTarget.length === 0) || (rankChk && suitChk)){
-            tTarget.push(activeCard.pop());
+               while(activeCard.length > 0){
+                   tTarget.push(activeCard.shift());
+               }
             console.log(tTarget);
         }
         render();
@@ -133,24 +132,24 @@ function addWaste(e) {
     render();
 }
 function selectCard(e){
+    console.log(e.target.parentNode);
     var inArr = e.target.parentNode.className;
     if(inArr === 'tableau'){
-        var tColumn = e.target.parentNode.id.charAt(1);
-        var canSelect = tableau[tColumn][tableau[tColumn].length-1];
+        var tColumn = e.target.id.charAt(0);
+        var tRow = e.target.id.charAt(1);
+        var canSelect = tableau[tColumn][tRow];
+        console.log(tColumn);
+        console.log(tRow);
+        console.log(canSelect);
+
         if(!activeCard.length && canSelect.isActive) {
-            console.log(activeCard);
-            console.log(canSelect);
-            console.log('if1');
-            activeCard.push(tableau[e.target.parentNode.id.charAt(1)].pop());
+            activeCard = tableau[tColumn].splice(tRow, tableau[tColumn].length-tRow);
         }
-        if(!activeCard.length && !canSelect.isActive){
-            console.log(activeCard);
-            console.log(canSelect);
-            console.log('if2');
-         canSelect.isActive = true;
+        if(!activeCard.length && canSelect === tableau[tColumn][tableau[tColumn].length-1]){
+            canSelect.isActive = true;
         }
     } else if(inArr === 'waste' && !activeCard.length){
-        activeCard.push(waste.pop());
+         activeCard.push(waste.pop());
     } else if(inArr === 'foundation'){
         return foundation;
     }
@@ -158,15 +157,14 @@ function selectCard(e){
 }
 function chkWin() {
     if (foundation[0].length === foundation[1].length === foundation[2].length === foundation[3].length === 13) {
-        return 'winner winner chicken dinner';
-    } else {
-        return 'you lose';
+        console.log('winner winner chicken dinner');
     }
 }
 init();
-render();
 
-//b moving cards multiple within the tableau
-//d click and drag functionality
-//e css image library link
-//f general styling
+//add king to empty tableau column
+//* setting a lose condition
+//b click and drag functionality
+//c css image library link
+//d general styling
+//e undo button
