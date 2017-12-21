9f6859eb3e7a244d3a69bfc1bf0d579857f68c38
Nick Taylor <nicholasjt22@gmail.com>
2017-12-21 00:53:50 -0800

    correct waste logic

--- js/app.js
+++ js/app.js
@@ -9,8 +9,9 @@ var foundationEl = document.getElementById("foundation");
 var wasteEl = document.getElementById("waste");
 var stockEl = document.getElementById("stock");
 /*----- event listeners -----*/
-document.getElementById('game').addEventListener('click', selectCard);
+document.getElementById('tableau').addEventListener('click', selectTableau);
 stockEl.addEventListener('click', addWaste);
+wasteEl.addEventListener('click', selectWaste)
 document.getElementById('restart').addEventListener('click',init);
 tableauEl.addEventListener('click', addTableau);
 foundationEl.addEventListener('click', addFoundation);
@@ -36,9 +37,9 @@ function render(){
         }
         var i=0;
         for(card in tableau[col]) {
-
-           if(tableau[col][card].isActive){ 
-               tChildren[col].innerHTML = `${tChildren[col].innerHTML}<div id='${h}${i}'class='cards'>${tableau[col][card].rank} of ${tableau[col][card].suit}</div>`;
+           if(tableau[col][card].isActive){
+               tChildren[col].innerHTML = `${tChildren[col].innerHTML}<div id='${h}${i}'class='cards'></div>`;
+               tChildren[col].lastChild.style.backgroundImage = "url("+tableau[col][card].imgLink+")"; 
             } else { 
                 tChildren[col].innerHTML = `${tChildren[col].innerHTML}<div id='${h}${i}'class='cards'></div>`;
             }
@@ -46,23 +47,24 @@ function render(){
         } 
         h++;    
     }
-    var count = 0;
     while(wasteEl.childElementCount>0){
          wasteEl.removeChild(wasteEl.firstChild);
-         count++;
     } 
-    var a = 0;   
     for(card in waste) {
         waste[card].isActive = false;
-        if (card > waste.length-4) {
-            wasteEl.innerHTML = wasteEl.innerHTML +`<div id='7${a}' class='cards'>${waste[card].rank} of ${waste[card].suit}</div>`;
-            if(Number(card) === waste.length-1) waste[card].isActive = true;
-            a++;    
+        if (card >= waste.length-3) {
+            //console.log(waste[0]);
+            wasteEl.innerHTML = wasteEl.innerHTML +`<div id='7${card}' class='cards'></div>`;
+            wasteEl.lastChild.style.backgroundImage = "url("+waste[card].imgLink+")";
+            // console.log(wChildren[card])
+            if(Number(card) === waste.length-1) waste[card].isActive = true; 
         }
     }
     for(arr in foundation) {
         if(!foundation[arr].length){
-            document.getElementById(arr+'f').innerHTML = '';
+            document.getElementById(arr+'f').style.backgroundImage = "url(none)";
+        } else {
+            document.getElementById(arr+'f').style.backgroundImage = "url("+foundation[arr][foundation[arr].length-1].imgLink+")"
         }
     }       
     chkWin();
@@ -71,7 +73,31 @@ class Card {
     constructor(suit, rank) {
 	    this.suit = suit;
 	    this.rank = rank;
-	    this.isActive = false;
+        this.isActive = false;
+        this.rankTranslator = function() {
+           var rankName;
+           switch(this.rank){
+               case 1:
+                   rankName = 'A';
+                   break;
+               case 10:
+                   rankName = 'r'+rank;
+                   break;    
+               case 11:
+                   rankName = 'J';
+                   break;
+               case 12:
+                   rankName = 'Q';
+                   break;
+               case 13:
+                   rankName = 'K';
+                   break;
+               default:
+                   rankName = 'r0'+this.rank;
+           }
+           this.imgLink = './css/img/' + this.suit + '/'+this.suit+'-'+ rankName+'.svg';
+       };
+       this.rankTranslator();     
     }
 }
 function makeDeck(){
@@ -108,7 +134,7 @@ function addFoundation(e) {
                 activeCard.push(fTarget[fTarget.length-1].pop())
             }
         if  (isRank && isSuit) {
-                e.target.innerHTML = `${activeCard[0].rank} of ${activeCard[0].suit}`;
+                e.target = `${activeCard[0].rank} of ${activeCard[0].suit}`;
                 fTarget.push(activeCard.pop());
             }
             render();    
@@ -126,23 +152,25 @@ function addTableau(e) {
         var rankChk = activeCard[0].rank+1 === tTarget[tTarget.length-1].rank;
         var suitChk = suit.indexOf(activeCard[0].suit)%2 !== suit.indexOf(tTarget[tTarget.length-1].suit)%2;
         if((rankChk && suitChk)){
-               while(activeCard.length > 0){
+            while(activeCard.length > 0){
                    tTarget.push(activeCard.shift());
-               }
-               render();
             }
+            render();
+        }
     } 
 }
 function addWaste(e) {
-    if(stock.length === 0) {
-        stock = stock.concat(waste.reverse());
-        waste = []; 
-    } else {
-        waste = waste.concat(stock.splice(stock.length-3).reverse());
+    if(!activeCard.length){
+        if(stock.length === 0) {
+            stock = stock.concat(waste.reverse());
+            waste = []; 
+        } else {
+            waste = waste.concat(stock.splice(stock.length-3).reverse());
+        }
     }
     render();
 }
-function selectCard(e){
+function selectTableau(e){
     if(!activeCard.length){
         var inArr = e.target.parentNode.className;
         if(inArr === 'tableau'){
@@ -155,25 +183,25 @@ function selectCard(e){
             if(!activeCard.length && canSelect === tableau[tColumn][tableau[tColumn].length-1]){
                 canSelect.isActive = true;
             }
-        } else if(inArr === 'waste' && !activeCard.length){
-           activeCard = waste.splice(waste.length-(e.target.id.charAt(1), e.target.parentNode.childElementCount-e.target.id.charAt(1)));
-        } else if(inArr === 'foundation'){
-
         }
+    }
+
+    render();
+}
+function selectWaste(e){
+    if(!activeCard.length){
+        activeCard.push(waste.pop());
     }
     render();
 }
 function chkWin() {
-    if (foundation[0].length === foundation[1].length === foundation[2].length === foundation[3].length === 13) {
-        console.log('winner winner chicken dinner');
-    }
+    if (foundation[0].length === foundation[1].length === foundation[2].length === foundation[3].length === 13) {console.log('winner winner chicken dinner');}
 }
 init();
 
-
-//a setting a lose condition
-//b css image library link
-//c click and drag functionality
-//d general styling
-//e undo button
-// peer review checklist
+//b logical bug with multiple  cards from the waste array, single digit id lookup bug
+//c setting a lose condition
+//d click and drag functionality
+//e peer review checklist
+//* general styling
+//* e undo button
